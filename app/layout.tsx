2ba0bca22ffd7a98cd4f86a92489bf65a12c99e0
ArseniyJam <arseniyyy63@gmail.com>
2025-04-15 02:08:19 +0300

    deploy7

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/footer/footer";
 import React from "react";
 import { getMe } from "@/services/get-me";
 import { getCartLength } from "@/data/cart";
+import Link from "next/link";
 
 export const metadata: Metadata = {
    title: "Create Next App",
@@ -16,14 +17,11 @@ export default async function RootLayout({
 }: Readonly<{
    children: React.ReactNode;
 }>) {
-   const user = await getMe();
-   const cartLength = await getCartLength();
-
    return (
       <html lang="en">
          <body className={`text-dark bg-white `}>
             <div className={`px-4 xl:px-[100px] flex flex-col`}>
-               <Header cartLength={cartLength} user={user} />
+               <Link href="/">Home</Link>
                <div
                   className={`min-h-[300px] lg:min-h-[500px] 2xl:min-h-[600px] flex flex-col`}
                >
